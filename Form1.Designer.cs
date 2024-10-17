2f79c34b1ff28c481da0e64526094901a51bbbe2
NgXuanThanh <kmaxtjr234@gmail.com>
2024-10-17 15:53:37 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace SignUp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.user_box = new System.Windows.Forms.TextBox();
+            this.email_box = new System.Windows.Forms.TextBox();
+            this.pass_box = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 38);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ĐĂNG KÝ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // user_box
+            // 
+            this.user_box.Location = new System.Drawing.Point(154, 95);
+            this.user_box.Multiline = true;
+            this.user_box.Name = "user_box";
+            this.user_box.Size = new System.Drawing.Size(258, 30);
+            this.user_box.TabIndex = 5;
+            this.user_box.TextChanged += new System.EventHandler(this.user_box_TextChanged);
+            // 
+            // email_box
+            // 
+            this.email_box.Location = new System.Drawing.Point(154, 163);
+            this.email_box.Multiline = true;
+            this.email_box.Name = "email_box";
+            this.email_box.Size = new System.Drawing.Size(258, 32);
+            this.email_box.TabIndex = 6;
+            this.email_box.TextChanged += new System.EventHandler(this.email_box_TextChanged);
+            // 
+            // pass_box
+            // 
+            this.pass_box.Location = new System.Drawing.Point(154, 233);
+            this.pass_box.Multiline = true;
+            this.pass_box.Name = "pass_box";
+            this.pass_box.PasswordChar = '*';
+            this.pass_box.Size = new System.Drawing.Size(258, 30);
+            this.pass_box.TabIndex = 7;
+            this.pass_box.TextChanged += new System.EventHandler(this.pass_box_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 26);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "User:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 26);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Password:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(34, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 26);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Email:";
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignUp.Location = new System.Drawing.Point(154, 312);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(117, 36);
+            this.btnSignUp.TabIndex = 11;
+            this.btnSignUp.Text = "Đăng ký";
+            this.btnSignUp.UseVisualStyleBackColor = true;
+            this.btnSignUp.Click += new System.EventHandler(this.btnSignUp_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(292, 312);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(107, 36);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 395);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSignUp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pass_box);
+            this.Controls.Add(this.email_box);
+            this.Controls.Add(this.user_box);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox user_box;
+        private System.Windows.Forms.TextBox email_box;
+        private System.Windows.Forms.TextBox pass_box;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignUp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void user_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }     
+
+        private void email_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pass_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, pattern);
+        }
+
+        private bool IsValidPassword(string password)
+        {
+            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+            return Regex.IsMatch(password, pattern);
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            if(user_box.Text =="" || email_box.Text =="" || pass_box.Text =="")
+            {
+                MessageBox.Show("Vui lòng nhập thông tin!","Thông báo");
+            }
+            else if(!IsValidEmail(email_box.Text))
+            {
+                MessageBox.Show("Email không đúng định dạng!", "Thông báo");
+            }
+            else if (!IsValidPassword(pass_box.Text))
+            {
+                MessageBox.Show("Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt!", "Thông báo");
+            }
+            else
+            {
+                MessageBox.Show("Đăng ký thành công!","Thông báo");
+            }
+        }
+    }
+}
